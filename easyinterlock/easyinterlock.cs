b12d185d57dcf60206174d8e9698a7452876817e
CRH380B-6216L <muner_szr6216@outlook.com>
2023-09-16 18:31:39 +0800

    添加项目文件。

---
+++ easyinterlock/easyinterlock.cs
@@ -0,0 +1,48 @@
+﻿namespace EasyInterlock
+{
+    public class Clock
+    {
+        public DateTime Current { get; set; }
+        public int ClockRate { get; set; }
+        public DateTime? SavedAt { get; set; }
+
+        public Clock() 
+        {
+            Current = DateTime.Now;
+            ClockRate = 1;
+        }
+
+        public Clock(DateTime startDate) : this() 
+        {
+            Current = startDate;
+        }
+
+        public Clock(DateTime startDate, int clockRate) : this(startDate)
+        {
+            ClockRate = clockRate;
+        }
+
+        public void Up()
+        {
+            if (SavedAt != null)
+            {
+                TimeSpan elapsed = (TimeSpan)(DateTime.Now - SavedAt) * ClockRate;
+                Current.Add(elapsed);
+            }
+            SavedAt = null;
+        }
+
+        public void Down(bool isContinueUponDown)
+        {
+            if (isContinueUponDown)
+            {
+                SavedAt = DateTime.Now;
+            }
+        }
+
+        public void Tick(int interval)
+        {
+            Current = Current.AddMilliseconds(interval * ClockRate);
+        }
+    }
+}
